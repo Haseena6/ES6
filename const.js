d19a17c1ea240223b5446b3a98116be2df1b76cb
Haseena6 <96560884+Haseena6@users.noreply.github.com>
2022-03-28 11:25:36 +0530

    Update const.js

--- const.js
+++ const.js
@@ -2,7 +2,7 @@ function display(num)
 {
     const value = 100;
     console.log("before assigning : "+value);
-    value = num; //cant modify once its declared and initialized
+    value = num;
     console.log("after assigning : "+value);
 }
 display(200);
