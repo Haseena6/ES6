36e320bdaf4b7dcfddbac77fca7e6ac9aefb0a11
Haseena6 <96560884+Haseena6@users.noreply.github.com>
2022-03-30 12:35:51 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+const finalParticipants = ['hasee', 'agayu', 'shifa', 'Natasha', 'Bobby'];
+ 
+const announcements = finalParticipants.map(member => {
+  return member + ' joined the contest.';
+})
+ 
+console.log(announcements);
---
+++ spread.js
@@ -0,0 +1,5 @@
+let colors = ['Red', 'Yellow'];  
+let newColors = [...colors];  
+newColors.push('Green');  
+console.log(newColors);  
+console.log(colors); 
